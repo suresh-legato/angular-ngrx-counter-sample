d0fb0cf4d29c9b82f592aa6491ef26548e5adda8
B, Sureshkumar <Sureshkumar.B@carelon.com>
2023-12-13 22:09:58 +0530

    feat: adds code to unsubscrib the counter state

--- src/app/components/counter-display/counter-display.component.ts
+++ src/app/components/counter-display/counter-display.component.ts
@@ -1,23 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { CounterModel } from '../../shared/store/counter.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-counter-display',
   templateUrl: './counter-display.component.html',
   styleUrl: './counter-display.component.scss',
 })
-export class CounterDisplayComponent implements OnInit {
+export class CounterDisplayComponent implements OnInit, OnDestroy {
   counterDisplay!: number;
   channelName!: string;
+  counterSubscription!: Subscription;
 
   constructor(private store: Store<{ counter: CounterModel }>) {}
 
   ngOnInit(): void {
-    this.store.select('counter').subscribe((data) => {
-      console.log('data', data);
-      this.counterDisplay = data.counter;
-      this.channelName = data.channelName;
-    });
+    this.counterSubscription = this.store
+      .select('counter')
+      .subscribe((data) => {
+        console.log('data', data);
+        this.counterDisplay = data.counter;
+        this.channelName = data.channelName;
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.counterSubscription) {
+      this.counterSubscription.unsubscribe();
+    }
   }
 }
