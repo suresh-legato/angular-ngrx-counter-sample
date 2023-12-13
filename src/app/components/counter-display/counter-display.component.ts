685dcea4190373d737db8a0ba6346ec2002d274d
B, Sureshkumar <Sureshkumar.B@carelon.com>
2023-12-13 22:38:30 +0530

    feat: adds selectors

--- src/app/components/counter-display/counter-display.component.ts
+++ src/app/components/counter-display/counter-display.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { CounterModel } from '../../shared/store/counter.model';
 import { Observable, Subscription } from 'rxjs';
+import { getCounter } from '../../shared/store/counter.selector';
 
 @Component({
   selector: 'app-counter-display',
@@ -11,25 +12,25 @@ import { Observable, Subscription } from 'rxjs';
 export class CounterDisplayComponent implements OnInit, OnDestroy {
   counterDisplay!: number;
   channelName!: string;
-  // counterSubscription!: Subscription;
+  counterSubscription!: Subscription;
   counter$!: Observable<CounterModel>;
 
   constructor(private store: Store<{ counter: CounterModel }>) {}
 
   ngOnInit(): void {
-    // this.counterSubscription = this.store
-    //   .select('counter')
-    //   .subscribe((data) => {
-    //     console.log('data', data);
-    //     this.counterDisplay = data.counter;
-    //     this.channelName = data.channelName;
-    //   });
-    this.counter$ = this.store.select('counter');
+    this.counterSubscription = this.store
+      .select(getCounter)
+      .subscribe((data) => {
+        console.log('data', data);
+        this.counterDisplay = data;
+      });
+    console.log('CounterDisplayComponent');
+    // this.counter$ = this.store.select('counter');
   }
 
   ngOnDestroy(): void {
-    // if (this.counterSubscription) {
-    //   this.counterSubscription.unsubscribe();
-    // }
+    if (this.counterSubscription) {
+      this.counterSubscription.unsubscribe();
+    }
   }
 }
--- src/app/components/custom-counter/custom-counter.component.ts
+++ src/app/components/custom-counter/custom-counter.component.ts
@@ -1,22 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { customIncrement } from '../../shared/store/counter.action';
 import { CounterModel } from '../../shared/store/counter.model';
+import { Subscription } from 'rxjs';
+import { getChannelName } from '../../shared/store/counter.selector';
 
 @Component({
   selector: 'app-custom-counter',
   templateUrl: './custom-counter.component.html',
   styleUrl: './custom-counter.component.scss',
 })
-export class CustomCounterComponent {
+export class CustomCounterComponent implements OnInit, OnDestroy {
   counterInput!: number;
   actionType: string = 'add';
+  channelName!: string;
+  counterSubscription!: Subscription;
 
   constructor(private store: Store<{ counter: CounterModel }>) {}
+
+  ngOnInit(): void {
+    this.counterSubscription = this.store
+      .select(getChannelName)
+      .subscribe((data) => {
+        console.log('data', data);
+        this.channelName = data;
+        console.log('CustomCounterComponent');
+      });
+  }
 
   onCustomIncrement() {
     this.store.dispatch(
       customIncrement({ value: +this.counterInput, action: this.actionType })
     );
+  }
+
+  ngOnDestroy(): void {
+    if (this.counterSubscription) this.counterSubscription.unsubscribe();
   }
 }
