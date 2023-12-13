a14781358b322bb31e529527c226cf7dfa6c9c5c
B, Sureshkumar <Sureshkumar.B@carelon.com>
2023-12-13 22:15:00 +0530

    feat: uses observable to bind store value to UI

--- src/app/components/counter-display/counter-display.component.ts
+++ src/app/components/counter-display/counter-display.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { CounterModel } from '../../shared/store/counter.model';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-counter-display',
@@ -11,23 +11,25 @@ import { Subscription } from 'rxjs';
 export class CounterDisplayComponent implements OnInit, OnDestroy {
   counterDisplay!: number;
   channelName!: string;
-  counterSubscription!: Subscription;
+  // counterSubscription!: Subscription;
+  counter$!: Observable<CounterModel>;
 
   constructor(private store: Store<{ counter: CounterModel }>) {}
 
   ngOnInit(): void {
-    this.counterSubscription = this.store
-      .select('counter')
-      .subscribe((data) => {
-        console.log('data', data);
-        this.counterDisplay = data.counter;
-        this.channelName = data.channelName;
-      });
+    // this.counterSubscription = this.store
+    //   .select('counter')
+    //   .subscribe((data) => {
+    //     console.log('data', data);
+    //     this.counterDisplay = data.counter;
+    //     this.channelName = data.channelName;
+    //   });
+    this.counter$ = this.store.select('counter');
   }
 
   ngOnDestroy(): void {
-    if (this.counterSubscription) {
-      this.counterSubscription.unsubscribe();
-    }
+    // if (this.counterSubscription) {
+    //   this.counterSubscription.unsubscribe();
+    // }
   }
 }
